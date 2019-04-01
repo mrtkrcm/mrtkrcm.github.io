b7f51b7853c44dc65a2fb7228f9dff6665931c05
Carlos Aller <caller@dminc.com>
2019-04-01 17:44:30 +0200

    Improved log
    Change-Id: Ie16bb237336d9e457f3107a57d21654c9ba52e2e

--- scripts/releaseAllSharedComponents.sh
+++ scripts/releaseAllSharedComponents.sh
@@ -7,7 +7,7 @@ allComponents=$(cd ${sharedComponentsDir} && ls -d1 */ | cut -d\/ -f1 )
 
 for component in $allComponents; do
     echo "Processing component ${component}. Contents of the folder:"
-    ls -lah ${sharedComponentsDir}${componentName}
+    ls -lah ${sharedComponentsDir}${component}
     componentCheckSum=$(cd ${sharedComponentsDir} &&  tar -cf - ${component} | md5sum  | cut -d\  -f1)
     echo "Checksum for component ${component} is $componentCheckSum"
     checksumFileContents="<empty>"
