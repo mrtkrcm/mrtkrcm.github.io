824d8b70829e6477af1cffa6ad3cc6ebac3dad65
Carlos Aller <caller@dminc.com>
2019-04-01 17:09:48 +0200

    Added skeleton of EventForm component
    Change-Id: I2a97e1b36b482cd6dcfcaba2a1c113e6212daac7

---
+++ sandbox/src/shared-components/dmi-mch-event-form/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventForm = () => (
+    <>
+      <label for="eventName">Title: </label>
+      <input type="text" name="eventName"/>
+    <>)
+
+export default EventForm
