5879e99fef2862f6980783fb2f77df6d0f6b8b89
Carlos Aller <caller@dminc.com>
2019-04-01 17:18:54 +0200

    Use Fragment, because <> is not supported
    Change-Id: I483e4fffd06159030f8061df152367412ad99d37

--- sandbox/src/shared-components/dmi-mch-event-form/index.js
+++ sandbox/src/shared-components/dmi-mch-event-form/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const EventForm = () => (
-    <>
+    <Fragment>
       <label for="eventName">Title: </label>
       <input type="text" name="eventName"/>
-    <>)
+    </Fragment>)
 
 export default EventForm
