faa78cef8ee19b9ef9dface6751474ed74aa4ca1
Josep Dominguez <jdominguez@dminc.com>
2019-04-01 15:07:34 +0200

    MCHGB-2749: Shared Components - Project setup
    Change-Id: I3a845d576a4e8e7bd751c0feb15edb388cfc6bcf

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import './App.css'
+
+function Index() {
+  return <h2>Home Page</h2>;
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h2>
+              MCH Sandbox
+            </h2>
+            <Link to='/'>Home</Link>
+            <Link to='/hello-world'>Hello World</Link>
+          </header>
+        </div>
+        <Route path="/" exact component={Index} />
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
