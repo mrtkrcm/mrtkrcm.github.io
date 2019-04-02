ac0d44a9b4eb75c32819e8ee0127a911af5c6672
Josep Dominguez <jdominguez@dminc.com>
2019-04-02 10:00:12 +0200

    Project setup
    Change-Id: Ib3a350d28d8142bd4ca23a002fb2a1b4cc017539

--- sandbox/src/shared-components/dmi-mch-event-form/index.js
+++ sandbox/src/shared-components/dmi-mch-event-form/index.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react'
-import { Input, Form, Container, Grid, TextArea } from 'semantic-ui-react'
+import React from 'react'
+import { Input, Form, Container, Grid, TextArea, Control } from 'semantic-ui-react'
 
 const EventForm = () => (
   <Container>
@@ -9,31 +9,19 @@ const EventForm = () => (
         <Grid.Row columns={2}>
           <Grid.Column>
             <Form.Field>
-              <label>Enter Name</label>
-              <Input type='text' />
+              <Control>
+                <label>Enter Name</label>
+                <Input type='text' />
+              </Control>
             </Form.Field>
           </Grid.Column>
           
           <Grid.Column>
             <Form.Field>
-              <label>Image</label>
-              <Input type='file' />
-            </Form.Field>
-          </Grid.Column>
-          
-        </Grid.Row>
-        <Grid.Row columns={2}>
-          <Grid.Column>
-            <Form.Field>
-              <label>Subtitle</label>
-              <Input type='text' />
-            </Form.Field>
-          </Grid.Column>
-          
-          <Grid.Column>
-            <Form.Field>
-              <label>Image Caption</label>
-              <Input type='text' />
+              <Control>
+                <label>Image</label>
+                <Input type='file' />
+              </Control>
             </Form.Field>
           </Grid.Column>
           
@@ -41,8 +29,30 @@ const EventForm = () => (
         <Grid.Row columns={2}>
           <Grid.Column>
             <Form.Field>
-              <label>Long Description</label>
-              <TextArea type='text' />
+              <Control>
+                <label>Subtitle</label>
+                <Input type='text' />
+              </Control>
+            </Form.Field>
+          </Grid.Column>
+          
+          <Grid.Column>
+            <Form.Field>
+              <Control>
+                <label>Image Caption</label>
+                <Input type='text' />
+              </Control>
+            </Form.Field>
+          </Grid.Column>
+          
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <Form.Field>
+              <Control>
+                <label>Long Description</label>
+                <TextArea type='text' />
+              </Control>
             </Form.Field>
           </Grid.Column>
         </Grid.Row>
