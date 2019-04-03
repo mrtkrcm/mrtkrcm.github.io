24e450e130b50feb2aac00f318150657f68df4b7
Josep Dominguez <jdominguez@dminc.com>
2019-04-03 17:51:42 +0200

    Dmi shared lib test
    Change-Id: Ib0fe7bcc6d33055fa381acf4e4dfd81c70075951

--- scripts/releaseSharedComponent.sh
+++ scripts/releaseSharedComponent.sh
@@ -15,7 +15,7 @@ echo "Releasing ${componentName}"
 ${basedir}/version/closeVersion.sh ${componentName}
 
 cp -R $sharedComponentsDir$componentName $temporaryDirectory
-(cd $temporaryDirectory/$componentName && npm install && npm run build && cp package.json build/ && cd build/ && npm publish && rm -rf $temporaryDirectory)
+(cd $temporaryDirectory/$componentName && npm install && npm run build && cp package.json dist/ && cd dist/ && npm publish && rm -rf $temporaryDirectory)
 
 if [ $? == 0 ]; then
     echo "NPM publish was OK. Increasing to next snapshot version."
@@ -23,13 +23,6 @@ if [ $? == 0 ]; then
     git add ${sharedComponentsDir}${componentName}/package.json
     git commit -m "JENKINS: Closed release `${basedir}/version/getVersion.sh ${componentName}` of ${componentName}"
     git push origin HEAD:master
-
-    echo "Cleaning generated files"
-    rm -rf ${sharedComponentsDir}${componentName}/build
-    rm -rf ${sharedComponentsDir}${componentName}/node-modules
-    rm -rf ${sharedComponentsDir}${componentName}/package-lock.json
-    echo "Cleaning done. Contents of the folder: "
-    ls -lah ${sharedComponentsDir}${componentName}
 else
     echo "NPM build failed."
     exit 1;
