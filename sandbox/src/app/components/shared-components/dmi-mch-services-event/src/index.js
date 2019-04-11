014965eaa248fa4ee3a263f920fb40f994b859cd
Josep Dominguez <jdominguez@dminc.com>
2019-04-11 15:54:49 +0200

    Events service, remove extra space

--- sandbox/src/app/components/shared-components/dmi-mch-services-event/src/index.js
+++ sandbox/src/app/components/shared-components/dmi-mch-services-event/src/index.js
@@ -2,7 +2,7 @@ import qs from 'query-string'
 
 /**
  * This class contains the services for Events. The constructor requires a context to be passed.
- * When using the Event Class, it will require a context to be passed. The context contains the 
+ * When using the Event Class, it will require a context to be passed. The context contains the
  * http object, which is specific for each environment
  */
 class Event {
