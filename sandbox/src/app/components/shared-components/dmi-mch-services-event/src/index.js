d3cfd236a69493a061129a7c58e4039254292ea0
Josep Dominguez <jdominguez@dminc.com>
2019-04-11 15:49:29 +0200

    Events service

---
+++ sandbox/src/app/components/shared-components/dmi-mch-services-event/src/index.js
@@ -0,0 +1,21 @@
+import qs from 'query-string'
+
+/**
+ * This class contains the services for Events. The constructor requires a context to be passed.
+ * When using the Event Class, it will require a context to be passed. The context contains the 
+ * http object, which is specific for each environment
+ */
+class Event {
+  constructor(context) {
+    this.context = context
+  }
+
+  getAttributes() {
+    const params = {
+      attributeType: 'EVENT_TYPE'
+    }
+    return this.context.http.get(`/msvc/v1/events/attributes/?${qs.stringify(params)}`)
+  }
+}
+
+export default Event
