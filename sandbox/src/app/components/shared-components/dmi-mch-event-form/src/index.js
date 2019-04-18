cdd3bdb9c95c18fb4b518749de16ecd76ad0a46b
Josep Dominguez <jdominguez@dminc.com>
2019-04-18 21:55:07 +0200

    MCHGB-2657: [FE] Add / Edit Event Form across MFP & CMS

--- sandbox/src/app/components/shared-components/dmi-mch-event-form/src/index.js
+++ sandbox/src/app/components/shared-components/dmi-mch-event-form/src/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-for */
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import 'regenerator-runtime'
 import { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { withFormik } from 'formik'
