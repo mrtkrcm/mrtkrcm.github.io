dd5e350b3bb7e12228446e15d99ee96848adec56
Josep Dominguez <jdominguez@dminc.com>
2019-05-15 09:39:24 +0200

    New Component: Input Feedback, for error messages

---
+++ sandbox/src/app/components/shared-components/dmi-mch-utils-inputfeedback/src/index.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const InputFeedback = ({ className, children }) => (<small className={className}>{children}</small>)
+
+InputFeedback.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string
+}
+
+const InputFeedbackStyled = styled(InputFeedback)`
+  display: block;
+  margin-top: 8px;
+  color: #ff1341;
+  font-size: 12px;
+`
+
+export default InputFeedbackStyled
