03e36fa847b4d72b2f26d92952d0f722f854f9e0
Josep Dominguez <jdominguez@dminc.com>
2019-05-15 12:46:39 +0200

    New package: Labels service
    Change-Id: Ic064f0d699eea0b67504265badea86a17c7563b2

---
+++ sandbox/src/app/components/shared-components/dmi-mch-services-labels/src/index.js
@@ -0,0 +1,16 @@
+class Label {
+  constructor(context) {
+    this.context = context
+    this.headers = { 'cache-control': 'no-cache' }
+  }
+
+  get = (entityId, fieldName) => this.context.http
+    .get(`/msvc/v1/translations/Label/${entityId}/${fieldName}`, null, this.headers)
+
+  set = (entityType, entityId, fieldName, locale, value) => this.context.http
+    .put(`/msvc/v1/translations/${entityType}/${entityId}/${fieldName}/${locale}`, value)
+
+  getAllLabels = () => this.context.http.get('/msvc/v1/translations/Label?entityType=Label', null, this.headers)
+}
+
+export default Label
