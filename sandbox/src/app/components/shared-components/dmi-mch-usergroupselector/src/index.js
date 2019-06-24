1e3dad5f2bd8a9dcbf92a22b555746704b229ce2
Jean-Pierre CHENG <jcheng@dminc.com>
2019-06-24 09:20:16 +0700

    MCHGB-3445: [Events & Exhibitions] Duplicate api request when open event detail

--- sandbox/src/app/components/shared-components/dmi-mch-usergroupselector/src/index.js
+++ sandbox/src/app/components/shared-components/dmi-mch-usergroupselector/src/index.js
@@ -1,34 +1,8 @@
 import PropTypes from 'prop-types'
-import React, { useState, useEffect } from 'react'
 import { Form } from 'semantic-ui-react'
-import ValidateAxiosResponse from 'dmi-mch-utils-validate-axios-response'
-import { buildSelectOptions } from 'dmi-mch-utils-dropdown'
-import Logger from 'dmi-mch-utils-logger'
-import Usergroup from 'dmi-mch-services-usergroup'
 
 const UserGroupSelector = (props) => {
-  const [userGroupsDropDown, setUserGroupsDropDown] = useState([])
-  const { context, selected, name, setFieldValue, label, disabled } = props
-  useEffect(() => {
-    const userGroup = new Usergroup(context)
-    const fetchUsergroups = async () => {
-      try {
-        const userGroups = await userGroup.getAll()
-        if (ValidateAxiosResponse(userGroups)) {
-          const selectItem = {
-            key: 'name',
-            value: 'name',
-            text: 'displayName'
-          }
-          const dropdownOptions = buildSelectOptions(userGroups.data, selectItem)
-          setUserGroupsDropDown(dropdownOptions)
-        }
-      } catch (e) {
-        Logger(e)
-      }
-    }
-    fetchUsergroups()
-  }, [context])
+  const { selected, name, setFieldValue, label, disabled, dropDownOptions } = props
 
   return (
     <Form.Select
@@ -37,7 +11,7 @@ const UserGroupSelector = (props) => {
       name={name}
       fluid
       label={label}
-      options={userGroupsDropDown}
+      options={dropDownOptions}
       onChange={(e, { value }) => {
         setFieldValue(name, value)
       }}
@@ -48,12 +22,12 @@ const UserGroupSelector = (props) => {
 }
 
 UserGroupSelector.propTypes = {
-  context: PropTypes.object,
   selected: PropTypes.array,
   name: PropTypes.string,
   label: PropTypes.string,
   setFieldValue: PropTypes.func,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  dropDownOptions: PropTypes.array
 }
 
 export default UserGroupSelector
